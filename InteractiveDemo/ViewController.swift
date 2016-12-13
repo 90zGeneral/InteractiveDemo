485e8385259006826af2abf56314a4aad5adb29f
Roydon Jeffrey <brushdam14823@gmail.com>
2016-12-13 14:50:26 -0500

    Working with alerts and sharesheet

--- InteractiveDemo/ViewController.swift
+++ InteractiveDemo/ViewController.swift
@@ -10,14 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var spinner: UIActivityIndicatorView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        spinner.stopAnimating()
+        spinner.hidesWhenStopped = true
+        
+        /*
+         
+        Timer.scheduledTimer(withTimeInterval: 4, repeats: false, block: { timer in
+            
+            self.spinner.stopAnimating()
+            
+        })
+        
+        //Alert View
+        let alert = UIAlertController(title: "Greetings", message: "There was an error", preferredStyle: .actionSheet)
+        alert.addTextField(configurationHandler: { tf in
+            tf.backgroundColor = UIColor.red
+        })
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: { _ in
+            self.spinner.startAnimating()
+        })
+        alert.addAction(okAction)
+        
+        present(alert, animated: true, completion: nil)
+         
+        */
+        
+        
+        //Share Sheet View
+        let shareSheet = UIActivityViewController(activityItems: ["Hello there"], applicationActivities: nil)
+        
+        present(shareSheet, animated: true, completion: {
+            print("sharesheet showing up")
+        })
+        
     }
 
 
